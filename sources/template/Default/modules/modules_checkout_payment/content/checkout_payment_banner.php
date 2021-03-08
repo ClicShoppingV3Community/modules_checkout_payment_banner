70674e4f68e3c530198e3d714e663c939db9bcbd
ClicShopping <Yae8mr2o*>
2021-03-08 16:33:54 +0100

    update for 3.300 version

--- sources/template/Default/modules/modules_checkout_payment/content/checkout_payment_banner.php
+++ sources/template/Default/modules/modules_checkout_payment/content/checkout_payment_banner.php
@@ -1,4 +1,4 @@
 <div class="col-md-<?php echo $content_width; ?>">
   <div class="separator"></div>
-  <div class="text-md-center moduleCheckoutPaymentCommentBanner"><?php echo $payment_process_order_banner; ?></div>
+  <div class="text-center moduleCheckoutPaymentCommentBanner"><?php echo $payment_process_order_banner; ?></div>
 </div>
