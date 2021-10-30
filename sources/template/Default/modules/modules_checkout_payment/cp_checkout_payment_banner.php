a17222333fd0b45326f77e1431226cb13dd66856
ClicShopping <admin@clicshopping.org>
2021-10-30 11:49:26 +0200

    update

--- sources/template/Default/modules/modules_checkout_payment/cp_checkout_payment_banner.php
+++ sources/template/Default/modules/modules_checkout_payment/cp_checkout_payment_banner.php
@@ -17,8 +17,8 @@
   class cp_checkout_payment_banner {
     public string $code;
     public string $group;
-    public string $title;
-    public string $description;
+    public $title;
+    public $description;
     public ?int $sort_order = 0;
     public bool $enabled = false;
 
@@ -41,7 +41,7 @@
       $CLICSHOPPING_Service = Registry::get('Service');
       $CLICSHOPPING_Banner = Registry::get('Banner');
 
-      if (isset($_GET['Checkout']) && isset($_GET['Billing'])) {
+      if (isset($_GET['Checkout'], $_GET['Billing'])) {
 
         $content_width = (int)MODULE_CHECKOUT_PAYMENT_BANNER_CONTENT_WIDTH;
         $payment_process_order_banner = '';
